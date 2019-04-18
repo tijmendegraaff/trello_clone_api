4a526427c5cbbb3862ad57b9c766192e73ae82cd
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-18 11:43:06 +0200

    add response to /login route

--- src/routes/user.js
+++ src/routes/user.js
@@ -15,12 +15,13 @@ router.post("/users", async (req, res) => {
   }
 });
 
-router.post("users/login", async (req, res) => {
+router.post("/users/login", async (req, res) => {
   try {
     const user = await User.findByCredentials(
       req.body.email,
       req.body.password
     );
+    res.send(user);
   } catch (e) {
     res.status(500).send();
   }
