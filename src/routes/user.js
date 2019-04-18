ba00d80cba66d16767de5a2f332d6c1efc75191a
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-18 11:30:39 +0200

    setup user login route

--- src/routes/user.js
+++ src/routes/user.js
@@ -15,4 +15,15 @@ router.post("/users", async (req, res) => {
   }
 });
 
+router.post("users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 module.exports = router;
