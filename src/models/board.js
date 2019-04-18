5cfddfff67d447eacdc4da49369f177e7781bb96
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-18 13:11:51 +0200

    add board model

---
+++ src/models/board.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const boardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Board = mongoose.model("Board", boardSchema);
+
+module.exports = Board;
