016c18fdbb43a6e84f1ac0ad3c46be601dbd938b
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-18 11:12:20 +0200

    setup post user route

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 require("./db/mongoose");
+// Import Routes
+const userRouter = require("./routes/user");
 
 const app = express();
 
 app.use(express.json());
+app.use(userRouter);
 
 module.exports = app;
