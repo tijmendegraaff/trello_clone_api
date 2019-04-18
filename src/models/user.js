05f8636e978d2931ffb0233de9c6141a3caff0b6
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-18 11:23:17 +0200

    add password hashing before saving user

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -36,6 +37,17 @@ const userSchema = new mongoose.Schema(
     timestamps: true
   }
 );
+
+// Password hashing before user save
+userSchema.pre("save", async function(next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 10);
+  }
+
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
